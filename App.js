0aba563b7686dfb52863a1e4629c3cf2cb808042
Antonio Henriques de França Terceiro <antoniomed1.an@gmail.com>
2021-09-27 09:20:59 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react';
+import {Platform, Text, StyleSheet, View} from 'react-native';
+import params from './src/params';
+import Field from './src/components/Field';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mine!</Text>
+        <Text style={styles.instructions}>
+          Tamanho da grade: {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+        <Field />
+        <Field opened />
+        <Field opened nearMines={1} />
+        <Field opened nearMines={2} />
+        <Field opened nearMines={3} />
+        <Field opened nearMines={6} />
+        <Field mined />
+        <Field mined opened />
+        <Field mined opened exploded />
+        <Field flagged />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    fontSize: 15,
+    textAlign: 'center',
+    margin: 2,
+  },
+});
